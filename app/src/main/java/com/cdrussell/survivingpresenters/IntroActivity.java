fea1d722333c97b34c52e994619fc03368c61700
CDRussell <craig.russell.1986@gmail.com>
2016-08-15 00:18:37 +0100

    make view adhere to MVP view interface, show how to retain presenter

--- app/src/main/java/com/cdrussell/survivingpresenters/IntroActivity.java
+++ app/src/main/java/com/cdrussell/survivingpresenters/IntroActivity.java
@@ -1,13 +1,62 @@
 package com.cdrussell.survivingpresenters;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity implements IntroMvp.View {
+
+    private IntroMvp.Presenter presenter;
+
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        bindViews();
+        attachPresenter();
+    }
+
+    private void bindViews() {
+        output = (TextView) findViewById(R.id.presenter_details);
+    }
+
+    private void attachPresenter() {
+        presenter = (IntroMvp.Presenter) getLastCustomNonConfigurationInstance();
+        if (presenter == null) {
+            presenter = new IntroPresenter();
+        }
+        presenter.attachView(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.detachView();
+        super.onDestroy();
+    }
+
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        return presenter;
+    }
+
+    public void startButtonPressed(View view) {
+        presenter.startCounting();
+    }
+
+    public void stopButtonPressed(View view) {
+        presenter.stopCounting();
+    }
+
+    @Override
+    public void updateCount(final int count) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                output.setText(getString(R.string.updated_count, count));
+            }
+        });
     }
 }
